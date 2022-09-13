98625f5a6ab6e5c2801a101e0c65f21b26847343
NullAspiration <173g1993@gmail.com>
2022-09-13 17:36:40 -0400

    changes

--- Lab_03/Class2.cs
+++ Lab_03/Class2.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +14,13 @@ namespace Lab_03
 {
     public class SSN
     {
+        Random FirstDigits = new Random();
+        Random SecondDigits = new Random();
+        Random ThirdDigits = new Random();
 
+        int[] DummySSN;
+
+    
     }
+
 }
