2c57bb4df3e926d6946c69e6aa1e9a57875d8189
NullAspiration <173g1993@gmail.com>
2022-09-13 17:14:09 -0400

    Created Class SSN

---
+++ Lab_03/Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//invalid parameter for SSN
+//Source: https://secure.ssa.gov/poms.nsf/lnx/0110201035
+
+namespace Lab_03
+{
+    public class SSN
+    {
+
+    }
+}
